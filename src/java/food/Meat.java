23c51a840303f27004ffbb7f2422448da102a83d
paulhtrott <design@ninjawebstudios.com>
2013-02-01 22:47:48 -0600

    Modified Meat to an abstract class, so that Meat objects cannot be instantiated.

--- src/java/food/Meat.java
+++ src/java/food/Meat.java
@@ -2,186 +2,127 @@ package food;
 
 /**
  * 
- * This class will hold all meat specific data.
+ * This abstract class for all meat specific data.
  * Implements from the Food interface class.
+ * All methods are abstract having to be implemented by inheriting classes.
  * 
  * @author Paul Trott (ptrott)
  * Date: January 26, 2013
  */
 
-public class Meat implements IFood {
-    
-    //Instance variable
-    private Food food;
-    
-    /**
-     * no-arg Default Constructor
-     */
-    public Meat(){
-        food = new Food();
-    }
-    
-    /**
-     * Overloaded Constructor
-     * @param name
-     * @param description
-     * @param calories
-     * @param caloriesFromFat
-     * @param servingSize
-     * @param protein
-     * @param fat
-     * @param carbs 
-     */
-    public Meat(String name, String description, int calories, int caloriesFromFat, 
-            double servingSize, double protein, double fat, double carbs){
-        food = new Food(name, description, calories, caloriesFromFat, 
-                servingSize, protein, fat, carbs);
-    }
-    
+public abstract class Meat implements IFood {
     
     /**
      * Setter for name.
      * @param name 
      */
     @Override
-    public void setName(String name){
-        food.setName(name);
-    }
+    public abstract void setName(String name);
     
     /**
      * Getter for name.
      * @return name
      */
     @Override
-    public String getName(){
-        return food.getName();
-    }
+    public abstract String getName();
 
     /**
      * Getter for calories
      * @return 
      */
     @Override
-    public int getCalories() {
-        return food.getCalories();
-    }
+    public abstract int getCalories();
 
     /**
      * Setter for calories
      * @param calories 
      */
     @Override
-    public void setCalories(int calories) {
-        food.setCalories(calories);
-    }
-
+    public abstract void setCalories(int calories);
+            
     /**
      * Getter for caloriesFromFat
      * @return  caloriesFromFat
      */
     @Override
-    public int getCaloriesFromFat() {
-        return food.getCaloriesFromFat();
-    }
+    public abstract int getCaloriesFromFat();
 
     /**
      * Setter for caloriesFromFat
      * @param caloriesFromFat 
      */
     @Override
-    public void setCaloriesFromFat(int caloriesFromFat) {
-        food.setCaloriesFromFat(caloriesFromFat);
-    }
+    public abstract void setCaloriesFromFat(int caloriesFromFat);
 
     /**
      * Getter for servingSize.
      * @return servingSize
      */
     @Override
-    public double getServingSize() {
-        return food.getServingSize();
-    }
+    public abstract double getServingSize();
 
     /**
      * Setter for servingSize
      * @param servingSize 
      */
     @Override
-    public void setServingSize(double servingSize) {
-        food.setServingSize(servingSize);
-    }
+    public abstract void setServingSize(double servingSize);
 
     /**
      * Getter for protein
      * @return protein
      */
     @Override
-    public double getProtein() {
-        return food.getProtein();
-    }
+    public abstract double getProtein();
 
     /**
      * Setter for protein
      * @param protein 
      */
     @Override
-    public void setProtein(double protein) {
-        food.setProtein(protein);
-    }
+    public abstract void setProtein(double protein);
 
     /**
      * Getter for fat
      * @return fat
      */
     @Override
-    public double getFat() {
-        return food.getFat();
-    }
+    public abstract double getFat();
 
     /**
      * Setter for fat
      * @param fat 
      */
     @Override
-    public void setFat(double fat) {
-        food.setFat(fat);
-    }
+    public abstract void setFat(double fat);
 
     /**
      * Getter for carbs
      * @return carbs
      */
     @Override
-    public double getCarbs() {
-        return food.getCarbs();
-    }
+    public abstract double getCarbs();
 
     /**
      * Setter for carbs
      * @param carbs 
      */
     @Override
-    public void setCarbs(double carbs) {
-        food.setCarbs(carbs);
-    }
+    public abstract void setCarbs(double carbs);
 
     /**
      * Getter for description
      * @return description
      */
     @Override
-    public String getDescription() {
-        return food.getDescription();
-    }
+    public abstract String getDescription();
 
     /**
      * Setter for description
      * @param description 
      */
     @Override
-    public void setDescription(String description) {
-        food.setDescription(description);
-    }
+    public abstract void setDescription(String description);
     
         
 }
