400d5a190280f87ed6b713890322a4ce70a7e1f2
paulhtrott <design@ninjawebstudios.com>
2013-02-07 18:21:16 -0600

    Added ConnectionPool class to database package

---
+++ src/java/database/ConnectionPool.java
@@ -0,0 +1,71 @@
+package database;
+
+import java.sql.*;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+
+/**
+ * The Connection Pool class for the derby database connection. A connection
+ * pool is needed to control database connections. Settings for the Class are
+ * set in the config xml file. Two static instance variables are assigned to the
+ * Class: ConnectionPool pool and DataSource dataSource. The class also contains
+ * a private constructor.
+ *
+ * @author Paul Trott (ptrott)
+ */
+public class ConnectionPool {
+
+    //Instance variables.
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+
+    /**
+     * private constructor that establishes a ConnectionPool for a derby
+     * database connection.
+     */
+    private ConnectionPool() {
+        try {
+            InitialContext ic = new InitialContext();
+            dataSource = (DataSource)ic.lookup("java:/comp/env/jdbc/healthbenefits");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Static method to get an instance of a ConnectionPool.
+     *
+     * @return pool - a ConnectionPool
+     */
+    public static ConnectionPool getInstance() {
+        if (pool == null) {
+            pool = new ConnectionPool();
+        }
+        return pool;
+    }
+    
+    /**
+     * Returns a connection object to be used for a connection to access the database.
+     * @return dataSource connection
+     */
+    public Connection getConnection(){
+        try{
+            return dataSource.getConnection();
+        }catch(SQLException sqle){
+            sqle.printStackTrace();
+            return null; 
+        }
+    }
+    
+    /**
+     * Used to close the Connection object.
+     * @param c - a connection to close.
+     */
+    public void freeConnection(Connection c){
+        try{
+            c.close();
+        } catch(SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+}
