c71a3f8cd7713e0978df219a556109348d296a07
paulhtrott <design@ninjawebstudios.com>
2013-02-14 21:19:32 -0600

    Added DBUtil to work with database connections.

---
+++ src/java/database/DBUtil.java
@@ -0,0 +1,59 @@
+package database;
+
+import java.sql.*;
+
+/**
+ * A class definition for a DBUtil class allowing for an easier way to 
+ * close database query statements.
+ * 
+ * This DBUtil Class is in the database package.
+ * 
+ * @author Paul Trott (ptrott)
+ * Date: February 9, 2013
+ */
+public class DBUtil {
+    
+    /**
+     * Close statement connections.
+     * @param s 
+     */
+    public static void closeStatement(Statement s){
+        try{
+            if(s != null){
+                s.close();
+            }
+            
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Close Prepared Statement connections.
+     * @param ps 
+     */
+    public static void closePreparedStatement(Statement ps){
+        try{
+            if(ps != null){
+                ps.close();
+            } 
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Close ResultSet connections.
+     * @param rs 
+     */
+    public static void closeResultSet(ResultSet rs){
+        try{
+            if(rs != null){
+                rs.close();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
