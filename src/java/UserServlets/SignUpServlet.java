dd910ae446689b858b4387fe2f652270e2b9f732
paulhtrott <design@ninjawebstudios.com>
2013-02-15 20:33:16 -0600

    Modified the SignupServlet to do Regex and Field processing and return an error if fields do not match web app requirements.

--- src/java/UserServlets/SignUpServlet.java
+++ src/java/UserServlets/SignUpServlet.java
@@ -16,8 +16,8 @@ import user.User;
 /**
  * Serlvet to process a user during sign up. The user signs up on the signup
  * page and user will be stored in an object, stored in the database; the
- * username, email and first name and will be stored in session cookies and session
- * attributes to be used throughout the web application.
+ * username, email and first name and will be stored in session cookies and
+ * session attributes to be used throughout the web application.
  *
  * @author Paul Trott (ptrott)
  */
@@ -40,68 +40,128 @@ public class SignUpServlet extends HttpServlet {
         String firstName = request.getParameter("firstname");
         String username = request.getParameter("username");
         String password = request.getParameter("password1");
+        String password2 = request.getParameter("password2");
         String emailAddress = request.getParameter("emailaddress");
+
+        //Variable to hold messages to be returned to user in 
+        //case of entered information is invalid.
+        String message = "null";
+
         //variable to hold returning url
-        String url = "";
+        String url;
         //varible for cookie age
         //seconds-minutes-hours-days-years
-        int cookieAge = 60*60*24*365*2; //2 years
+        int cookieAge = 60 * 60 * 24 * 365 * 2; //2 years
         //cookiePath to all of the application
         String cookiePath = "/";
 
         //Has and salt password before it is entered in the database.
         String hashed_pw = BCrypt.hashpw(password, BCrypt.gensalt(14));
-        
-        //Instantiate a User object of the signing up user.
-        User user = new User(firstName, username, hashed_pw, emailAddress);
 
         //Get a session object to store some user information in a session.
         HttpSession session = request.getSession();
 
         //Insert user into database if username and emailaddress doesn't already exist
         //in database.
-        if (UserData.isUserExisting(user.getEmailAddress(), user.getUsername())) {
-            //return to signin page.
-            url = "/signin.jsp";
+        if (UserData.isUserExisting(emailAddress, username)) {
+            //set message based on info inputed.
+            message = "Email address and/or username already exists.";
+            //synchronize message session attribute.
+            synchronized (session) {
+                session.setAttribute("message", message);
+            }
+            //return to signup page, show error.
+            url = "/signup.jsp";
+        } else if (!regex.Regex.isFirstNameValid(firstName)) {
+            //If entered name is not a valid name, show error message.
+            //set message.
+            message = "Please enter a valid first name, no numbers allowed.";
+            //synchronize message session attribute.
+            synchronized (session) {
+                session.setAttribute("message", message);
+            }
+            //return to signup page, show error.
+            url = "/signup.jsp";
+        } else if (firstName.length() > 50 && firstName.length() < 2) {
+            //If first name is greater than 50 and less than 2, show error message.
+            //set message.
+            message = "Please enter a first name, between 2 &amp; 50 characters.";
+            //synchronize message session attribute.
+            synchronized (session) {
+                session.setAttribute("message", message);
+            }
+            //return to signup page, show error.
+            url = "/signup.jsp";
+        } else if (username.length() < 6 && username.length() > 100) {
+            //If username is less than 6 and more than 100 characters long.
+            message = "Username must be between 5 &amp; 100 characters.";
+            //synchronize message session attribute.
+            synchronized (session) {
+                session.setAttribute("message", message);
+            }
+            //return to signup page, show error.
+            url = "/signup.jsp";
+
+        } else if (!(password2.equals(password))) {
+
+            //If password equals verification password2 and length is between 30 and 8
+            message = "Passwords do not equal.";
+            //synchronize message session attribute.
+            synchronized (session) {
+                session.setAttribute("message", message);
+            }
+            //return to signup page, show error.
+            url = "/signup.jsp";
+        } else if (password.length() > 30 && password.length() < 8) {
+            //If password is less than 8 and more than 30 characters long.
+            message = "Password must be between 8 &amp; 30 characters.";
+            //synchronize message session attribute.
+            synchronized (session) {
+                session.setAttribute("message", message);
+            }
+            //return to signup page, show error.
+            url = "/signup.jsp";
+        } else if (!regex.Regex.isEmailValid(emailAddress)) {
+            //If enetered email is not valid.
+            message = "Please enter a valid email address (johndoe@company.com).";
+            //synchronize message session attribute.
+            synchronized (session) {
+                session.setAttribute("message", message);
+            }
+            //return to signup page, show error.
+            url = "/signup.jsp";
         } else {
-            //set session attributes
+            //Instantiate a User object of the signing up user.
+            User user = new User(firstName, username, hashed_pw, emailAddress);
+            //set session attribute for first name in synchronized threads.
             synchronized (session) {
                 session.setAttribute("firstName", user.getFirstName());
-                session.setAttribute("username", user.getUsername());
-                session.setAttribute("emailAddress", user.getEmailAddress());
             }
-            //set cookies
-            Cookie firstNameCookie = new Cookie("firstNameCookie", user.getFirstName());
+
+            //set cookie for username.
             Cookie usernameCookie = new Cookie("usernameCookie", user.getUsername());
-            Cookie emailAddressCookie = new Cookie("emailAddressCookie", user.getEmailAddress());
-            
-            //Setup for first name cookie
-            firstNameCookie.setMaxAge(cookieAge);
-            firstNameCookie.setPath(cookiePath);
-            response.addCookie(firstNameCookie);
+
             //Setup for username cookie
             usernameCookie.setMaxAge(cookieAge);
             usernameCookie.setPath(cookiePath);
+            //usernameCookie.setSecure(true); //set to true when using SSL/HTTPS Certificate on production.
             response.addCookie(usernameCookie);
-            //Setup for the emailAddress cookie
-            emailAddressCookie.setMaxAge(cookieAge);
-            emailAddressCookie.setPath(cookiePath);
-            response.addCookie(emailAddressCookie);
-            
+
             //insert user into USERS table.
             UserData.insertUser(user);
             //insert same user into USERCODE table.
             UserData.addToUserCodeTable(user);
             //return user to signupthankyou.jsp
             url = "/index.jsp";
-            
+
         }
 
         //Code for forwarding user to new url view
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
         dispatcher.forward(request, response);
 
-
+        //To remove error message from page on a return to Signup page.
+        session.removeAttribute("message");
 
     }
 
