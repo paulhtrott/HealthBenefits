556cb7e30d9b9502ac8fdc63ce10af29970c6828
paulhtrott <design@ninjawebstudios.com>
2013-02-16 22:57:01 -0600

    Added a method to add food items to the database. Also added an overloaded method for items with specific types.

--- src/java/database/FoodData.java
+++ src/java/database/FoodData.java
@@ -1,6 +1,8 @@
 package database;
 
+import food.IFood;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,10 +27,118 @@ public class FoodData {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-
-    public String addFood() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    /**
+     * addFood adds food items to a database.
+     * Parameters are an IFood object, and two String objects for food code 
+     * and serving size measurement (cups ,ounces and grams).
+     * @param food
+     * @param foodCode
+     * @param servingSizeMeasurement
+     * @return returns the executed query results to the database.
+     */
+    public static int addFood(IFood food, String foodCode, String servingSizeMeasurement) {
+        
+        //Get a connection to the database through a connection pool.
+        //Create an instance of a pool and connection.
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        
+        //Instantitate a PreparedStatement
+        PreparedStatement statement = null;
+        
+        //Instantiate a query to add entered food to database.
+        String query = "INSERT INTO FOOD (FOODNAME, DESCRIPTION, CALORIES, "
+                + "CALORIESFROMFAT, SERVINGSIZE, PROTEIN, FAT, CARBS, "
+                + "SPECIFICFOODTYPE, FOOD_CODE, MEASUREMENT) VALUES "
+                + "(?,?,?,?,?,?,?,?,?,?,?)";
+        
+        try{
+            //Populate the statement with the food information
+            //pass in the query as a parameter.
+            statement = connection.prepareStatement(query);
+            statement.setString(1, food.getName());
+            statement.setString(2, food.getDescription());
+            statement.setInt(3, food.getCalories());
+            statement.setInt(4, food.getCaloriesFromFat());
+            statement.setDouble(5, food.getServingSize());
+            statement.setDouble(6, food.getProtein());
+            statement.setDouble(7, food.getFat());
+            statement.setDouble(8, food.getCarbs());
+            statement.setString(9,"null");
+            statement.setString(10, foodCode);
+            statement.setString(11, servingSizeMeasurement);
+            //Execute query.
+            return statement.executeUpdate();
+            
+            
+        }catch(SQLException sqle){
+            sqle.printStackTrace();
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(statement);
+            pool.freeConnection(connection);
+        }
+        
     }
+    
+    /**
+     * addFood adds food items to a database.
+     * Parameters are an IFood object, and two String objects for food code 
+     * and serving size measurement (cups ,ounces and grams), plus an extra String parameter
+     * specificType to store the specific type of food in the database.
+     * @param food
+     * @param foodCode
+     * @param servingSizeMeasurement
+     * @param specificType
+     * @return returns the executed query results to the database. 
+     */
+    public static int addFood(IFood food, String foodCode, 
+            String servingSizeMeasurement, String specificType) {
+        
+        //Get a connection to the database through a connection pool.
+        //Create an instance of a pool and connection.
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        
+        //Instantitate a PreparedStatement
+        PreparedStatement statement = null;
+        
+        //Instantiate a query to add entered food to database.
+        String query = "INSERT INTO FOOD (FOODNAME, DESCRIPTION, CALORIES, "
+                + "CALORIESFROMFAT, SERVINGSIZE, PROTEIN, FAT, CARBS, "
+                + "SPECIFICFOODTYPE, FOOD_CODE, MEASUREMENT) VALUES "
+                + "(?,?,?,?,?,?,?,?,?,?,?)";
+        
+        try{
+            //Populate the statement with the food information
+            //pass in the query as a parameter.
+            statement = connection.prepareStatement(query);
+            statement.setString(1, food.getName());
+            statement.setString(2, food.getDescription());
+            statement.setInt(3, food.getCalories());
+            statement.setInt(4, food.getCaloriesFromFat());
+            statement.setDouble(5, food.getServingSize());
+            statement.setDouble(6, food.getProtein());
+            statement.setDouble(7, food.getFat());
+            statement.setDouble(8, food.getCarbs());
+            statement.setString(9, specificType);
+            statement.setString(10, foodCode);
+            statement.setString(11, servingSizeMeasurement);
+            //Execute query.
+            return statement.executeUpdate();
+            
+            
+        }catch(SQLException sqle){
+            sqle.printStackTrace();
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(statement);
+            pool.freeConnection(connection);
+        }
+        
+    }
+    
+    
 
     /**
      * This request will get a list of all food types out of the Derby 
